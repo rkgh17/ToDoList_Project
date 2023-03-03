b13e245ef768c65a6fcbbcbac57b0b443998cb8e
hjh_home <rkgh17@gmail.com>
2023-03-03 11:38:54 +0900

    Front _ Signup Form _ pwValidation

--- todolist/front/src/routes/SignUp.js
+++ todolist/front/src/routes/SignUp.js
@@ -1,17 +1,94 @@
+import { useState } from "react";
+import "./Routes.css";
+
 function SignUp() {
+  // 이름, 아이디, 비밀번호
+  const [inputUserName, setUserName] = useState("");
+  const [inputUserId, setUserId] = useState("");
+  const [inputUserPw, setUserPw] = useState("");
+  const [inputUserPwCheck, setUserPwCheck] = useState("");
+
+  // 메시지 저장
+  const [passwordConfirmMessage, setPasswordConfirmMessage] = useState("");
+
+  // 유효성 검사 (일단 비밀번호만)
+  const [isPasswordConfirm, setIsPasswordConfirm] = useState(false);
+
+  const handleInputUserId = (e) => {
+    setUserId(e.target.value);
+  };
+  const handleInputUserName = (e) => {
+    setUserName(e.target.value);
+  };
+
+  const handleInputUserPw = (e) => {
+    setUserPw(e.target.value);
+  };
+
+  const handleInputUserPwCheck = (e) => {
+    setUserPwCheck(e.target.value);
+    if (inputUserPw === e.target.value) {
+      setPasswordConfirmMessage("비밀번호 일치");
+      setIsPasswordConfirm(true);
+    } else {
+      setPasswordConfirmMessage("비밀번호 불일치");
+      setIsPasswordConfirm(false);
+    }
+  };
+
+  const onClickSignUp = () => {};
+
   return (
     <div className="Main">
       <header className="Header">
-        <div>회원가입</div>
         <div style={{ display: "flex", flexDirection: "column" }}>
-          <label>Email </label>
-          <input type="email"></input>
-          <label>Name</label>
-          <input type="name"></input>
-          <label>비밀번호</label>
-          <input type="password"></input>
-          <label>비밀번호 확인</label>
-          <input type="password"></input>
+          <div>회원가입</div>
+
+          <div className="formbox">
+            <label>Name </label>
+            <input
+              type="name"
+              value={inputUserName}
+              onChange={handleInputUserName}
+            ></input>
+          </div>
+
+          <div className="formbox">
+            <label>Email (ID) </label>
+            <input
+              type="email"
+              value={inputUserId}
+              onChange={handleInputUserId}
+            ></input>
+          </div>
+          <div className="formbox">
+            <label>비밀번호 </label>
+            <input
+              type="password"
+              value={inputUserPw}
+              onChange={handleInputUserPw}
+              title="비밀번호"
+            ></input>
+          </div>
+
+          <div className="formbox">
+            <label>비밀번호 확인 </label>
+            <input
+              type="password"
+              value={inputUserPwCheck}
+              onChange={handleInputUserPwCheck}
+            ></input>
+            <span
+              className={`message ${isPasswordConfirm ? "success" : "error"}`}
+            >
+              {passwordConfirmMessage}
+            </span>
+          </div>
+        </div>
+        <div>
+          <button className="" onClick={onClickSignUp}>
+            회원가입
+          </button>
         </div>
       </header>
     </div>
