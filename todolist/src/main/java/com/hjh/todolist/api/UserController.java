cd2bf6020654cf0d8765828ea6e615277bad6e0a
hjh_home <rkgh17@gmail.com>
2023-03-05 15:12:28 +0900

    Front/Back _ axios api

--- todolist/src/main/java/com/hjh/todolist/api/UserController.java
+++ todolist/src/main/java/com/hjh/todolist/api/UserController.java
@@ -1,5 +1,24 @@
 package com.hjh.todolist.api;
 
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+	
+	@ResponseBody
+	@PostMapping("/api/signup")
+	public void signupForm(@RequestBody Map<String, Object> map) {
+		
+		// 폼 전송값 확인
+		System.out.println(map);
+		
+		// map 자료찾기
+		System.out.println(map.get("name"));
+	}
 
 }
