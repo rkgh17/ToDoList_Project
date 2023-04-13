0e42d6974bd8e930b69509c7fa3e41a3fd5f9885
hjh_home <rkgh17@gmail.com>
2023-04-13 14:32:14 +0900

    Front | TODO head 날짜

--- todolist/front/src/routes/Todo/TodoHead.js
+++ todolist/front/src/routes/Todo/TodoHead.js
@@ -12,7 +12,7 @@ const TodoHeadBlock = styled.div`
     font-size: 36px;
     color: #343a40;
   }
-  .day {
+  s .day {
     margin-top: 4px;
     color: #868e96;
     font-size: 21px;
@@ -26,10 +26,40 @@ const TodoHeadBlock = styled.div`
 `;
 
 function TodoHead() {
+  const today = new Date();
+
+  const week = (num) => {
+    switch (num) {
+      case 0:
+        return "일요일";
+        break;
+      case 1:
+        return "월요일";
+        break;
+      case 2:
+        return "화요일";
+        break;
+      case 3:
+        return "수요일";
+        break;
+      case 4:
+        return "목요일";
+        break;
+      case 5:
+        return "금요일";
+        break;
+      case 6:
+        return "토요일";
+        break;
+    }
+  };
+
   return (
     <TodoHeadBlock>
-      <h1>2019년 7월 10일</h1>
-      <div className="day">수요일</div>
+      <h1>
+        {today.getFullYear()}년 {today.getMonth() + 1}월 {today.getDate()}일
+      </h1>
+      <div className="day">{week(today.getDay())}</div>
       <div className="tasks-left">할 일 2개 남음</div>
     </TodoHeadBlock>
   );
