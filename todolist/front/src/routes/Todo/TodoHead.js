8e30dd1b785280b2b59d0a332f1881cf9984f321
hjh_home <rkgh17@gmail.com>
2023-04-26 13:08:25 +0900

    Front | TODO date

--- todolist/front/src/routes/Todo/TodoHead.js
+++ todolist/front/src/routes/Todo/TodoHead.js
@@ -27,39 +27,49 @@ const TodoHeadBlock = styled.div`
 
 function TodoHead() {
   const today = new Date();
+  const dateString = today.toLocaleDateString("ko-KR", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+  const dayName = today.toLocaleDateString("ko-KR", { weekday: "long" });
 
-  const week = (num) => {
-    switch (num) {
-      case 0:
-        return "일요일";
-        break;
-      case 1:
-        return "월요일";
-        break;
-      case 2:
-        return "화요일";
-        break;
-      case 3:
-        return "수요일";
-        break;
-      case 4:
-        return "목요일";
-        break;
-      case 5:
-        return "금요일";
-        break;
-      case 6:
-        return "토요일";
-        break;
-    }
-  };
+  // const week = (num) => {
+  //   switch (num) {
+  //     case 0:
+  //       return "일요일";
+  //       break;
+  //     case 1:
+  //       return "월요일";
+  //       break;
+  //     case 2:
+  //       return "화요일";
+  //       break;
+  //     case 3:
+  //       return "수요일";
+  //       break;
+  //     case 4:
+  //       return "목요일";
+  //       break;
+  //     case 5:
+  //       return "금요일";
+  //       break;
+  //     case 6:
+  //       return "토요일";
+  //       break;
+  //   }
+  // };
 
   return (
     <TodoHeadBlock>
       <h1>
-        {today.getFullYear()}년 {today.getMonth() + 1}월 {today.getDate()}일
+        {/* {today.getFullYear()}년 {today.getMonth() + 1}월 {today.getDate()}일 */}
+        {dateString}
       </h1>
-      <div className="day">{week(today.getDay())}</div>
+      <div className="day">
+        {/* {week(today.getDay())} */}
+        {dayName}
+      </div>
       <div className="tasks-left">해야 할 일</div>
     </TodoHeadBlock>
   );
